2d79c43298a9f6928b9a926b7b808a83a805e460
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-08-02 19:32:58 +1000

    Add initial files

---
+++ index.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('garden-control.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
