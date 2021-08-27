c61c8cd50056f6c2a313ef9f20ed463e3c4957f4
19932297 <19932297@student.westernsydney.edu.au>
2021-08-27 08:38:10 +0000

    add sleep function and scanGardenTest function

--- garden-control.js
+++ garden-control.js
@@ -1,4 +1,3 @@
-
 // NO VALUES FOR THESE VARIABLES IN FINAL BUILD!!!
 var TOKEN = "***REMOVED***";
 var EMAIL;
@@ -221,3 +220,77 @@ $.ajax(settings).done(function (response) {
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+
+ function sleep(d){
+    for(var t = Date.now();Date.now() - t <= d;);
+  }
+  
+function scanGardenTest () {
+	//move to the desired starting position
+	var x1= parseInt(document.getElementById("starting_xCoord").value);
+	var y1= parseInt(document.getElementById("starting_yCoord").value);
+	var x2= parseInt(document.getElementById("ending_xCoord").value);
+	var y2= parseInt(document.getElementById("ending_yCoord").value);
+	var z = 0;
+	var y_holder = y1;
+	//record the number of images taken
+	var n = 0;
+	var farmbot123 = new fbjs.Farmbot({ token: TOKEN });
+	//check current position on Y axis
+	while (y_holder<=y2) {
+		//initiate  x_holder's value
+		let x_holder = x1;
+		//check current position on X axis
+		while (x_holder <= x2){
+		//move to (x_holder,y_holder)
+		farmbot123
+		.connect()
+		.then( function () {
+      //sleep for 1 min
+	  		sleep(60000)
+			return farmbot123.moveAbsolute({ x: x_holder, y: y_holder, z: z, speed: 100 });
+		})
+		.then( function () {
+      //sleep for 1 min
+	  		sleep(60000)
+		return farmbot123.takePhoto({});
+		});
+		//increment the number of images taken by 1
+		n+=1;
+
+		//check if farmbot has taken 50 new images
+		if (n%400==0){
+		//save 400 new images
+		var settings = {
+			"url": "https://my.farm.bot/api/images",
+			"method": "GET",
+			"timeout": 0,
+			"headers": {
+			  "Authorization": "***REMOVED***",
+			  "Cookie": "***REMOVED***"
+		},};
+		  
+		  // Make var to store the response
+		  var savedResponse;		  
+		  // Acess the response and save it to the variable
+		  $.ajax(settings).done(function (response) {
+			  //console.log(response);
+				savedResponse = response;  
+				// x is the number of images the user want to download
+			    var NumOfImg = 400
+			    // 0 is the newest images, it will be downloaded first, then the second newest, and so on. 
+			    var ImgCounter = 0	  		  		  		  		  
+			    while (ImgCounter <= NumOfImg) {	  			  
+				    // Call python script and pass arguments to save the image to local folder
+				    const spawn = require("child_process").spawn;
+				    const pythonProcess = spawn('python',["saveImage.py", savedResponse[ImgCounter].attachment_url, savedResponse[ImgCounter].id]);
+				   i += 1;}});}
+		//increment x_holder by 50mm
+		x_holder += 50;
+	}
+		//increment y_holder by 50mm
+		y_holder += 50;	
+}
+}
+
