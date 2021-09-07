a60f822d69bdfc1e16bcb144595f337aca3deeba
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-09-08 00:45:20 +1000

    Add function to save plant data as csv

--- script.js
+++ script.js
@@ -98,25 +98,40 @@ function readCoordinates() {
 
 // Check current bot X Y Z
 function getPoints() {
+	const { Parser } = require('json2csv');
+	const fs = require('fs');
+
 	return new Promise((resolve, reject) => {
+		var settings = {
+			"url": "https://my.farmbot.io/api/points",
+			"method": "GET",
+			"timeout": 0,
+			"headers": {
+				"Authorization": "Bearer " + TOKEN,
+				"Access-Control-Allow-Origin": "*",
+				"Content-Type": "application/json"
+			},
+		};
 
-	var settings = {
-		"url": "https://my.farmbot.io/api/points",
-		"method": "GET",
-		"timeout": 0,
-		"headers": {
-			"Authorization": "Bearer " + TOKEN,
-			"Access-Control-Allow-Origin": "*",
-			"Content-Type": "application/json"
-		},
-	};
+		$.ajax(settings).done(function (response) {
+			// Filter out non-plants.
+			let plantDataJson = [];
 
-	$.ajax(settings).done(function (response) {
-		console.log(response);
-	}).then(function(response){
-		resolve(response);
-	});
-	
+			for (let i = 0; i < Object.keys(response).length; i++) {
+				if (response[i].pointer_type == "Plant") {
+					plantDataJson.push(response[i]);
+				}
+			}
+			JSON.stringify(plantDataJson);
+
+			// Save as csv.
+			const json2csvParser = new Parser();
+			const csv = json2csvParser.parse(plantDataJson);
+			
+			fs.writeFileSync('plant-data.csv', csv);
+		}).then(function(response){
+			resolve(response);
+		});
 	});
 }
 
