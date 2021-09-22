26f271bc447539355712b87b3f3065f5905aeece
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-09-23 03:17:45 +1000

    Update login page

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 $(document).ready(function() {
-	getSessionToken();
+	
 });
 
 let emailAdd,
@@ -10,19 +10,24 @@ userID;
 
 // DELETE THIS FUNC IN FINAL BUILD!!!
 function tempLogin() {
-	emailAdd = "***REMOVED***";
-	password = "***REMOVED***";
-	generateToken(emailAdd, password);
+	document.getElementById("email").value = "***REMOVED***";
+	document.getElementById("password").value = "***REMOVED***";
 }
 
 function saveCredentials() {
-emailAdd = document.getElementById("email").value;
+	emailAdd = document.getElementById("email").value;
 	password = document.getElementById("password").value;
 
+	// Check if there are other past users in the app.
 
+	// TODO: Check if email matches an existing user in db.
 
+	// Ask to merge if no match, otherwise do it automatically.
+
+	// If merging, update the db entry with new credentials.
+
+	// If not merging, create new user in db.
 	let db = new sqlite3.Database('./database/Chance_the_Gardener.db');
-	// Increment User_ID.
 
 	// Insert new user into table.
 	let sql = 'INSERT into User( Email, Password) values ("' + emailAdd + '", "' + password + '")';
@@ -39,6 +44,29 @@ emailAdd = document.getElementById("email").value;
 	// Close the database connection.
 	db.close();
 
+	// Generate a session token for this user with the API.
+	var settings = {
+		"url": "https://my.farmbot.io/api/tokens",
+		"method": "POST",
+		"timeout": 0,
+		"headers": {
+			"content-type": "application/json"
+		},
+		"data": JSON.stringify({
+			"user": {
+				"email": emailAdd,
+				"password": password
+			}
+		}),
+	};
+	$.ajax(settings).done(function (response) {
+		sessionToken = response.token.encoded;
+		console.log("Session token generated: " + sessionToken);
+	});
 
-	//generateToken(emailAdd, password);
+	// Save either the merged or new user ID as last logged in.
+	// userID = TODO: Get userID of the matching emailAdd from db.
+	// For testing purposes, this user ID will always be 1.
+	userID = 1;
+	localStorage.setItem('lastLoginUserID', userID);
 }
