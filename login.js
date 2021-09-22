9c2c3b3bc2ea7758e62f25460bd3687419bdd233
19932297 <19932297@student.westernsydney.edu.au>
2021-09-22 07:28:36 +0000

    after user enter email and password and hit login, they will saved into database.

--- login.js
+++ login.js
@@ -16,27 +16,16 @@ function tempLogin() {
 }
 
 function saveCredentials() {
-	// Grab credentials from form.
-	emailAdd = "***REMOVED***";
-	password = "***REMOVED***";
-	//emailAdd = document.getElementById("email").value;
-	//password = document.getElementById("password").value;
+emailAdd = document.getElementById("email").value;
+	password = document.getElementById("password").value;
 
-	// Check if there are other past users in the app.
 
-	// Check if email matches an existing user in db.
 
-	// Ask to merge if no match, otherwise do it automatically.
-
-	// If merging, update the db entry with no credentials.
-
-	// If not merging, create new user in db.
-	// Open the database connection.
 	let db = new sqlite3.Database('./database/Chance_the_Gardener.db');
 	// Increment User_ID.
 
 	// Insert new user into table.
-	let sql = 'INSERT into User(User_ID, Email, Password) values (1, "' + emailAdd + '", "' + password + '")';
+	let sql = 'INSERT into User( Email, Password) values ("' + emailAdd + '", "' + password + '")';
 	// Output the INSERT statement
 	console.log(sql);
 
@@ -50,11 +39,6 @@ function saveCredentials() {
 	// Close the database connection.
 	db.close();
 
-	// Save either the merged or new user ID as last logged in.
-	// For testing purposes, this user ID will always be 1.
-	userID = 1;
-	localStorage.setItem('lastLoginUserID', userID);
 
-
-	generateToken(emailAdd, password);
+	//generateToken(emailAdd, password);
 }
