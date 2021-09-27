799d021e5c1583280c6cef7e5493103961689995
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-09-28 00:12:50 +1000

    Connect Unity garden viewer to app

--- view-3d-garden.js
+++ view-3d-garden.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
 	createUserSelect();
 	createDateTimeSelect("renders", localStorage.getItem('lastLoginUserID'));
 });
+
+function launchGardenViewer() {
+	const child = require('child_process').execFile,
+	executablePath = "garden_viewer\\FarmBot 3D Viewer.exe";
+	//parameters = ["--input", "images", "--output", "renders", "--scale", "2"];
+
+	// child(executablePath, parameters, function (err, data) {
+	child(executablePath, function (err, data) {
+		console.log(err)
+		console.log(data.toString());
+	});
+}
