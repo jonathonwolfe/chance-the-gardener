24109cf94398acce758d1345060b1dcc7462cc78
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-10-02 17:00:00 +1000

    Removed old unused code

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ const db = require('electron-db');
 const fs = require('fs');
 const path = require('path');
 
-// NO VALUES FOR THESE VARIABLES IN FINAL BUILD!!!
-var gardenX=2700;
-var gardenY=1200;
-
 let lastLoggedInUserID,
 sessionToken;
 
@@ -57,118 +53,7 @@ function getSessionToken() {
 	}
 }
 
-// Delete later???
-function getUserName() {
-	$.ajax(settings).done(function (response) {
-		return response.user.name;
-	});
-}
-
-// Take 1 photo at current bot coordinates
-function takePhoto() {
-	var farmbot123 = new farmbot.Farmbot({ token: sessionToken });
-
-	farmbot123
-		.connect()
-		.then(function () {
-			return farmbot123.takePhoto({});
-		}).then(function (farmbot123) {
-			console.log("Photo taken");
-		}).catch(function (error) {
-			console.log("Something went wrong :(");
-		});
-}
-
-// Check current bot X Y Z
-function readCoordinates() {
-	return new Promise((resolve, reject) => {
-
-	var settings = {
-		"url": "https://my.farmbot.io/api/logs",
-		"method": "GET",
-		"timeout": 0,
-		"headers": {
-			"Authorization": "Bearer " + sessionToken,
-			"Access-Control-Allow-Origin": "*",
-			"Content-Type": "application/json"
-		},
-	};
-
-	$.ajax(settings).done(function (response) {
-		console.log(response[0].x, response[0].y, response[0].z);
-	}).then(function(response){
-		resolve(response[0].x + "," + response[0].y + "," + response[0].z);
-	});
-	
-	});
-}
-
-// Check current bot X Y Z
-function getPoints() {
-	const { Parser } = require('json2csv');
-
-	return new Promise((resolve, reject) => {
-		var settings = {
-			"url": "https://my.farmbot.io/api/points",
-			"method": "GET",
-			"timeout": 0,
-			"headers": {
-				"Authorization": "Bearer " + sessionToken,
-				"Access-Control-Allow-Origin": "*",
-				"Content-Type": "application/json"
-			},
-		};
-
-		$.ajax(settings).done(function (response) {
-			// Filter out non-plants.
-			let plantDataJson = [];
-
-			for (let i = 0; i < Object.keys(response).length; i++) {
-				if (response[i].pointer_type == "Plant") {
-					plantDataJson.push(response[i]);
-				}
-			}
-			JSON.stringify(plantDataJson);
-
-			// Save as csv.
-			const json2csvParser = new Parser();
-			const csv = json2csvParser.parse(plantDataJson);
-			
-			fs.writeFileSync('plant-data.csv', csv);
-		}).then(function(response){
-			resolve(response);
-		});
-	});
-}
-
-// Toggle LED light
-function toggleLight() {
-	var farmbot123 = new farmbot.Farmbot({ token: sessionToken });
-
-	farmbot123
-		.connect()
-		.then(function () {
-			return farmbot123.togglePin({ pin_number: 7 });
-		}).then(function (farmbot123) {
-			console.log("Light toggled");
-		}).catch(function (error) {
-			console.log("Something went wrong :(");
-		});
-}
-
-function checkLED(){
-	var device = new farmbot.Farmbot({ token: sessionToken });
-
-	// Check if device LED is ON >> Checking PIN 7
-	device.on("status", (state_tree) => {
-		console.log("LED status");
-		console.dir(state_tree.pins[7].value);
-	});
-}
-
-
-
-function setLEDon(){
+function setLEDon() {
 	var device = new farmbot.Farmbot({ token: sessionToken });
 
 
