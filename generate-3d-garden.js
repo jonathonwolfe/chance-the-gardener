eb38d1cc15c5e0d19245940c886bea68333d7987
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-10-05 03:08:29 +1100

    Layout for generate renders
    Also clean up script.js a bit.

--- generate-3d-garden.js
+++ generate-3d-garden.js
@@ -1,5 +1,59 @@
-$(document).ready(function() {
+$(document).ready(async function() {
 	getSessionToken();
 	createUserSelect();
 	await createDateTimeSelect("scans", parseInt(localStorage.getItem('lastLoginUserID')));
 });
+
+// Start rendering a set of images to create a 3D model
+function createRenders() {
+	// Check if renders folder exists yet, and create if not.
+	if (!fs.existsSync("./renders")) {
+		fs.mkdirSync("./renders");
+	}
+
+	// At start, setting the progress bar to 55%
+	var i = 0;
+	if (i == 0) {
+		i = 1;
+		var elem = document.getElementById("myBar");
+		var width = 1;
+		var id = setInterval(frame, 10);
+		function frame() {
+			if (width >= 55) {
+				clearInterval(id);
+				i = 0;
+			} else {
+				width++;
+				elem.style.width = width + "%";
+			}
+		}
+	}
+	// progress bar stops	
+
+	// execute meshroom application through command line
+	var child = require('child_process').execFile;
+	var executablePath = "Meshroom-2018.1.0\\meshroom_photogrammetry.exe";
+	var parameters = ["--input", "images", "--output", "renders", "--scale", "2"];
+
+	child(executablePath, parameters, function (err, data) {
+		console.log(err)
+		console.log(data.toString());
+
+		// set progress bar to 100% if process completed.
+		let str = data.toString();
+		let stage = 0;
+		stage = str.search("[13/13]")
+
+		if (stage != 0) {
+			var elem = document.getElementById("myBar");
+			var width = 1;
+			elem.style.width = 100 + "%";
+		}
+		// end
+
+	});
+}
+
+function createRender() {
+	
+}
--- script.js
+++ script.js
@@ -53,6 +53,7 @@ function getSessionToken() {
 	}
 }
 
+// TODO: Delete later.
 function setLEDon() {
 	var device = new farmbot.Farmbot({ token: sessionToken });
 
@@ -86,6 +87,7 @@ function setLEDon() {
 	  });
 }
 
+// TODO: Delete later.
 // Send log message to FarmBot system
 function sendLogMessage() {
 	var logValue = "Chance App: " + document.getElementById("logMessageText").value;
@@ -101,56 +103,6 @@ function sendLogMessage() {
 		}).catch(function (error) {
 			console.log("Something went wrong :(");
 		});
-}
-
-// Start rendering a set of images to create a 3D model
-function createRenders() {
-	// Check if renders folder exists yet, and create if not.
-	if (!fs.existsSync("./renders")) {
-		fs.mkdirSync("./renders");
-	}
-
-	// At start, setting the progress bar to 55%
-	var i = 0;
-	if (i == 0) {
-		i = 1;
-		var elem = document.getElementById("myBar");
-		var width = 1;
-		var id = setInterval(frame, 10);
-		function frame() {
-			if (width >= 55) {
-				clearInterval(id);
-				i = 0;
-			} else {
-				width++;
-				elem.style.width = width + "%";
-			}
-		}
-	}
-	// progress bar stops	
-
-	// execute meshroom application through command line
-	var child = require('child_process').execFile;
-	var executablePath = "Meshroom-2018.1.0\\meshroom_photogrammetry.exe";
-	var parameters = ["--input", "images", "--output", "renders", "--scale", "2"];
-
-	child(executablePath, parameters, function (err, data) {
-		console.log(err)
-		console.log(data.toString());
-
-		// set progress bar to 100% if process completed.
-		let str = data.toString();
-		let stage = 0;
-		stage = str.search("[13/13]")
-
-		if (stage != 0) {
-			var elem = document.getElementById("myBar");
-			var width = 1;
-			elem.style.width = 100 + "%";
-		}
-		// end
-
-	});
 }
 
 // Get list of folders in a directory.
