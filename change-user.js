260e1be67580db6dfbdb856c6c5b1494c8725c04
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-10-04 17:24:42 +1100

    Add db functionality to change user page

--- change-user.js
+++ change-user.js
@@ -3,15 +3,32 @@ $(document).ready(function() {
 	setUserDetails();
 });
 
-function setUserDetails() {
+async function setUserDetails() {
 	lastLoggedInUserID = parseInt(localStorage.getItem('lastLoginUserID'));
-	let userName, emailAdd;
 	
-	// Get user details from database.
+	// Get user details from database and API.
+	const currentUserObj = {userId: lastLoggedInUserID},
+	userCreds = await getDbRowWhere('user', currentUserObj),
+	emailAdd = userCreds[0].email;
 
+	var settings = {
+		"url": "https://my.farmbot.io/api/users",
+		"method": "GET",
+		"timeout": 0,
+		"headers": {
+			"Authorization": "Bearer " + sessionToken,
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/json"
+		},
+	};
 
 	// Set the user's details.
-	document.getElementById("current-user-name").innerHTML = userName;
+	$.ajax(settings).done(function (response) {
+		document.getElementById("current-user-name").innerHTML = response[0].name;
+	}).then(function(response){
+		resolve(response);
+	});
+
 	document.getElementById("current-email-add").innerHTML = emailAdd;		
 }
 
