cd7d07139d5c43f4dfb2a90fe24497bacb173136
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-10-09 20:17:16 +1100

    Add loading spinner and text to garden viewer launch

--- view-3d-garden.js
+++ view-3d-garden.js
@@ -5,9 +5,32 @@ $(document).ready(function() {
 function launchGardenViewer() {
 	const child = require('child_process').execFile,
 	executablePath = path.join(__dirname, 'garden_viewer', 'FarmBot 3D Viewer.exe');
+	const launchBtn = document.getElementById("launch-viewer-btn"),
+	loadMsg = document.getElementById("viewer-launch-msg"),
+	loadingSpinner = document.getElementById("viewer-launch-spinner");
+
+	// Disable launch button.
+	launchBtn.setAttribute("disabled", "");
+
+	// Show loading message.
+	loadMsg.classList.remove("d-none");
+	
+	// Show loading spinner.
+	loadingSpinner.classList.remove("d-none");
 
 	child(executablePath, function (err, data) {
 		console.log(err)
 		console.log(data.toString());
 	});
+
+	setTimeout(function () {
+		// Re-enable launch button.
+		launchBtn.removeAttribute("disabled");
+
+		// Hide loading message.
+		loadMsg.classList.add("d-none");
+		
+		// Hide loading spinner.
+		loadingSpinner.classList.add("d-none");
+	}, 15000);
 }
