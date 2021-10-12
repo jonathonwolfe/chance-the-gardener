84394fc77914fe38ea3864fa634fa3cd8de80c56
Jessica Lim <18377415@student.westernsydney.edu.au>
2021-10-12 18:38:49 +1100

    Add logging

--- manage-scan-results.js
+++ manage-scan-results.js
@@ -351,11 +351,13 @@ async function normalImport() {
 	} else if (importType == 'garden_viewer') {
 		importEmail = firstFilePath.split('/')[4];
 	}
+	log.info('importEmail: ' + importEmail);
 
 	// Create new user entry in db.
 	const newUserCredsObj = {email: importEmail, password: ''};
 	await addDbTableRow('user', newUserCredsObj);
 
+	log.info('import...done??');
 	// TODO: Toast import complete.
 }
 
